3928b1680e2fc923c5f0e380e720a28230b05201
Krishna Sai Cholkar <krishnasai3cks@gmail.com>
2020-10-14 12:07:02 +0530

    added image search api

--- Take_Home_Projects/Image Search Abstraction Layer/index.js
+++ Take_Home_Projects/Image Search Abstraction Layer/index.js
@@ -1,15 +1,15 @@
-
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/routes.js');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-const dotenv = require('dotenv');
 app.get('/',(req,res)=>{
     res.sendFile(process.cwd()+'/views/index.html');
 });
 
-MongoClient.connect(process.env.DATABASE,{useUnifiedTopology: true},(err,client)=>{
+MongoClient.connect(process.env.DATABASE,{ useUnifiedTopology: true },(err,client)=>{
+    if(err) console.log(err);
     var db = client.db('ImageSearch');
     (db)? console.log("success"): console.log("failure");
     routes(app,db);
--- Take_Home_Projects/Image Search Abstraction Layer/routes/routes.js
+++ Take_Home_Projects/Image Search Abstraction Layer/routes/routes.js
@@ -1,8 +1,29 @@
-module.exports = function(app,db){
-    app.get('/api/imagesearch/:query?offset=10',(req,res)=>{
-        res.send("happy birthday");
+var fetchJSON = require('fetch-json');
+var nodeFetch = require('node-fetch')
+module.exports = function(app, db) {
+  app.get("/api/imagesearch/:name", (req, res) => {
+    var offset = req.query.offset ? req.query.offset : 10;
+    var imageName = req.params.name;
+    var date = new Date().toISOString();
+    var testPrint = {
+      term: imageName,
+      when: date
+    };
+    var url = `https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/${imageName}?offset=${offset}`;
+    fetchJSON.get(url).then((data)=>{
+      db.collection("ImageSearch").insertOne(testPrint, (err, doc) => {
+        if (err) console.log(err);
+        else res.json(data);
+      });
+      
+    })
+  });
+  app.get("/api/imagesearch", (req, res) => {
+    db.collection("ImageSearch").find({}).toArray((err,data)=>{
+      if(err) console.log(err);
+      else{
+        res.json(data);
+      }
     });
-    app.get('/api/imagesearch',(req,res)=>{
-        res.send('happy birthday to you ');
-    });
-}
+  });
+};
